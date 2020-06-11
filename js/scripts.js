e792f3d802f739015d1d2959065ad689024fd1bb
Owaish Kalim <owaishkalim@gmail.com>
2020-06-11 17:04:47 +0530

    NPM Scripts

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+    $('#mycarousel').carousel({
+        interval: 1000
+    });    // this is not working, whereas if we set the data-interval attribute in mycarousel div, its working!
+    $('#carouselButton').click(function(){
+        if($('#carouselButton').children('span').hasClass('fa fa-pause')){
+            $('#mycarousel').carousel('pause');
+            $('#carouselButton').children('span').removeClass('fa fa-pause');
+            $('#carouselButton').children('span').addClass('fa fa-play');
+        }
+        else if($('#carouselButton').children('span').hasClass('fa fa-play')){
+            $('#mycarousel').carousel('cycle');
+            $('#carouselButton').children('span').removeClass('fa fa-play');
+            $('#carouselButton').children('span').addClass('fa fa-pause');
+        }
+    });
+
+    $('#login').click(function(){
+        $('#loginModal').modal('show');
+    });
+
+    $('#reserveTable').click(function(){
+        $('#reserveModal').modal('show');
+    });
+
+
+});
